b420d2bf395b92282e2157e1f6ab8493999d7afe
Alcides Rosario <alcidesborges01@gmail.com>
2026-08-16 19:32:15 +0200

    Corrigir URL do backend para o Render

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
    o backend no seu VPS. Enquanto testa no seu computador,
    deixe como está (http://localhost:4000/api).
 ========================================================= */
-const API_BASE = 'https://contafacil-backend.onrender.com/api';
+const API_BASE = 'https://conta-facil-mz-projecto.onrender.com/api';
 
 let authToken = null;
 
